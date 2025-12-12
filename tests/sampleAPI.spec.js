5195bf7be1c9f27654ebe1ae024029088838689b
rahul rahul <rahul1988gupta88@gmail.com>
2025-12-12 18:42:19 +0530

    Added taded for regression describe grouping block

--- tests/sampleAPI.spec.js
+++ tests/sampleAPI.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
-test.describe("Sample Regression API tests", () => {
+test.describe("Sample Regression API tests @reg", () => {
 
     let userId;
 
